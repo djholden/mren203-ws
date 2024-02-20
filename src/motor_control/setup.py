432c73b2983e3598d3287bb87f8d61bcafb2cb63
DJ Holden <dj.holden@queensu.ca>
2024-02-19 21:21:51 -0500

    fixed xml

--- src/motor_control/setup.py
+++ src/motor_control/setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'motor_driver=motor_control.motor_control_node:main' 
+            'motor_driver = motor_control.motor_control_node:main' 
         ],
     },
 )
